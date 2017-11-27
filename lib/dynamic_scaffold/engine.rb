49c826e9a814210758baf94fd8100456b6bbfe35
Masamoto Miyata <miyata@sincere-co.com>
2017-11-27 18:03:09 +0900

    Implement I18n.

--- lib/dynamic_scaffold/engine.rb
+++ lib/dynamic_scaffold/engine.rb
@@ -1,10 +1,11 @@
 module DynamicScaffold
   class Engine < ::Rails::Engine
     config.dynamic_scaffold = ActiveSupport::OrderedOptions.new
-    config.dynamic_scaffold.up_icon = '<i class="fa fa-chevron-up"></i>'.html_safe
-    config.dynamic_scaffold.down_icon = '<i class="fa fa-chevron-down"></i>'.html_safe
-    config.dynamic_scaffold.delete_icon = '<i class="fa fa-times"></i>'.html_safe
-    config.dynamic_scaffold.edit_icon = '<i class="fa fa-pencil"></i>'.html_safe
+    config.dynamic_scaffold.up_icon = '<i class="fa fa-chevron-up" aria-hidden="true"></i>'.html_safe
+    config.dynamic_scaffold.down_icon = '<i class="fa fa-chevron-down" aria-hidden="true"></i>'.html_safe
+    config.dynamic_scaffold.delete_icon = '<i class="fa fa-times" aria-hidden="true"></i>'.html_safe
+    config.dynamic_scaffold.edit_icon = '<i class="fa fa-pencil" aria-hidden="true"></i>'.html_safe
+    config.dynamic_scaffold.add_icon = '<i class="fa fa-plus" aria-hidden="true"></i>'.html_safe
 
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.assets.precompile += %w[dynamic_scaffold/*]
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace 'controls' do
-    scope 'master', as: :master do
-      get 'country', to: 'country#index'
-      get 'state', to: 'state#index'
+  scope "/:locale" do
+    namespace 'controls' do
+      scope 'master', as: :master do
+        get 'country', to: 'country#index'
+        get 'state', to: 'state#index'
+      end
     end
   end
 end
