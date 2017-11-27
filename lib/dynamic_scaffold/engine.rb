9ac1b7947dad512151838cf29825de4d7aa8be5f
Masamoto Miyata <miyata@sincere-co.com>
2017-11-27 18:28:12 +0900

    Move to icon config to `icon_set/font_awesome.rb`

--- lib/dynamic_scaffold/engine.rb
+++ lib/dynamic_scaffold/engine.rb
@@ -1,17 +1,14 @@
 module DynamicScaffold
   class Engine < ::Rails::Engine
     config.dynamic_scaffold = ActiveSupport::OrderedOptions.new
-    config.dynamic_scaffold.up_icon = '<i class="fa fa-chevron-up" aria-hidden="true"></i>'.html_safe
-    config.dynamic_scaffold.down_icon = '<i class="fa fa-chevron-down" aria-hidden="true"></i>'.html_safe
-    config.dynamic_scaffold.delete_icon = '<i class="fa fa-times" aria-hidden="true"></i>'.html_safe
-    config.dynamic_scaffold.edit_icon = '<i class="fa fa-pencil" aria-hidden="true"></i>'.html_safe
-    config.dynamic_scaffold.add_icon = '<i class="fa fa-plus" aria-hidden="true"></i>'.html_safe
+    config.dynamic_scaffold.icon_set = :font_awesome
 
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.assets.precompile += %w[dynamic_scaffold/*]
 
     config.after_initialize do |_app|
       require 'dynamic_scaffold/action_view'
+      require "dynamic_scaffold/icon_set/#{config.dynamic_scaffold.icon_set}"
     end
   end
 end
---
+++ lib/dynamic_scaffold/icon_set/font_awesome.rb
@@ -0,0 +1,7 @@
+config = ::Rails.application.config
+config.dynamic_scaffold.up_icon = '<i class="fa fa-chevron-up" aria-hidden="true"></i>'.html_safe
+config.dynamic_scaffold.down_icon = '<i class="fa fa-chevron-down" aria-hidden="true"></i>'.html_safe
+config.dynamic_scaffold.delete_icon = '<i class="fa fa-times" aria-hidden="true"></i>'.html_safe
+config.dynamic_scaffold.edit_icon = '<i class="fa fa-pencil" aria-hidden="true"></i>'.html_safe
+config.dynamic_scaffold.add_icon = '<i class="fa fa-plus" aria-hidden="true"></i>'.html_safe
+config.dynamic_scaffold.save_icon = '<i class="fa fa-hdd-o" aria-hidden="true"></i>'.html_safe
