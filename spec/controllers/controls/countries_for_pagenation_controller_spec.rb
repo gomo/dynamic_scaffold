62f04a034e7865501b9d334585dfb11f0cbe2f10
Masamoto Miyata <miyata@sincere-co.com>
2017-12-15 01:35:29 +0900

    Implement to return to the original page when returning from the edit page.

--- spec/controllers/controls/countries_for_pagenation_controller_spec.rb
+++ spec/controllers/controls/countries_for_pagenation_controller_spec.rb
@@ -8,5 +8,14 @@ RSpec.describe Controls::CountriesForPagenationController, type: :controller do
       expect(response.body.scan(/<li class="resplist-row js-item-row">/).size).to eq 1
       expect(response.body).to match(/<ul class="pagination/)
     end
+
+    it 'should return to the original page when you press the back button on the edit page.' do
+      countries = FactoryBot.create_list(:country, 3)
+      get :index, params: { locale: :en, page: 3 }
+      expect(response.body).to match(/href="\/en\/controls\/master\/countries_for_pagenation\/edit\?key%5Bid%5D=1&amp;page=3"/)
+
+      get :edit, params: { locale: :en, key: countries[2].primary_key_value, page: 3 }
+      expect(response.body).to match(/href="\/en\/controls\/master\/countries_for_pagenation\?page=3"/)
+    end
   end
 end
--- spec/dummy/app/controllers/controls/countries_for_pagenation_controller.rb
+++ spec/dummy/app/controllers/controls/countries_for_pagenation_controller.rb
@@ -3,7 +3,14 @@ module Controls
     include DynamicScaffold::Controller
     dynamic_scaffold Country do |c|
       c.list.sorter id: :asc
-      c.list.pagenation(per_page: 1)
+      c.list.pagenation(
+        per_page: 1,
+        window: 5,
+        outer_window: 1,
+        gap_buttons: true,
+        total_count: false,
+        end_buttons: false
+      )
     end
   end
 end
