91e42638f030dcf4fd4da6d7d93ba721ad832fb0
Masamoto Miyata <miyata@sincere-co.com>
2018-01-25 00:00:37 +0900

    Refactor sort animation logic.

--- app/assets/javascripts/dynamic_scaffold/sorter.js
+++ app/assets/javascripts/dynamic_scaffold/sorter.js
@@ -1,29 +1,31 @@
 document.addEventListener('dynamic_scaffold:load', function(){
   const promises = []
-  const resolvers = []
-  // Register the animation end event in all the rows.
-  Array.prototype.forEach.call(document.querySelectorAll('.dynamicScaffoldJs-item-row'), function(row){
-    row.addEventListener('transitionend', function(e){
-      if(e.target == row) resolvers.shift()(row)
-    })
-  })
 
-  function swapAnimation(source, target){
-    sourceRect = source.getBoundingClientRect()
-    targetRect = target.getBoundingClientRect()
-    
-    source.style.transition = 'all 200ms ease'
-    source.style.transform = 'translateY(' + (targetRect.top - sourceRect.top) + 'px)'
+  function swapAnimation(promises, source, target){
+    promises.push(new Promise(function(resolve){
+      source.sorting_animation_resolver = resolve
+      sourceRect = source.getBoundingClientRect()
+      targetRect = target.getBoundingClientRect()
+      
+      source.style.transition = 'all 200ms ease'
+      source.style.transform = 'translateY(' + (targetRect.top - sourceRect.top) + 'px)'      
+    }))
   }
 
-  function enableSortToButtons(buttons, otherSideAnimation, getTarget, swapElement){
+  /**
+   *
+   * @param NodeList button elements.
+   * @param Function Function that returns the element to move to.
+   * @param Function Function to register animation other than the source element.
+   * @param Function Function for swapping elements.
+   */
+  function addClickEvent(buttons, getTarget, otherSideAnimation, swapElement){
     Array.prototype.forEach.call(buttons, function(button){
       button.addEventListener('click', function(e){
         e.preventDefault()
         
         // Ignore while animating
         if(promises.length) return
-        if(resolvers.length) return
 
         const source = button.closest('.dynamicScaffoldJs-item-row')
         source.style.position = 'relative'
@@ -34,12 +36,11 @@ document.addEventListener('dynamic_scaffold:load', function(){
         if(!target) return
 
         // Start animation source to target
-        promises.push(new Promise(function(resolve){
-          resolvers.push(resolve)
-          swapAnimation(source, target)
-        }))
-
-        otherSideAnimation(promises, resolvers, source, target)
+        if(source != target){
+          swapAnimation(promises, source, target)
+        }
+        
+        otherSideAnimation(promises, source, target)
 
         // When both animations are finished
         Promise.all(promises).then(function(values){
@@ -56,60 +57,51 @@ document.addEventListener('dynamic_scaffold:load', function(){
     })
   }
 
-  enableSortToButtons(document.querySelectorAll('.dynamicScaffoldJs-sorter-top'), function(promises, resolvers, source, target){
-    do {
-      if(target.nextElementSibling){
-        promises.push(new Promise(function(resolve){
-          resolvers.push(resolve)
-          swapAnimation(target, target.nextElementSibling)
-        }))
-      }
-      target = target.nextElementSibling
+  function otherSideAnimationForUp(promises, source, target){
+    while(true){
       if(target == source) break
-    } while(target.nextElementSibling)
-  }, function(source){
+      swapAnimation(promises, target, target.nextElementSibling)
+      target = target.nextElementSibling
+    }
+  }
+
+  function otherSideAnimationForDown(promises, source, target){
+    while(true){
+      if(target == source) break
+      swapAnimation(promises, target, target.previousElementSibling)
+      target = target.previousElementSibling
+    }
+  }
+
+  // Register `transitionend` event in all the rows.
+  Array.prototype.forEach.call(document.querySelectorAll('.dynamicScaffoldJs-item-row'), function(row){
+    row.addEventListener('transitionend', function(e){
+      if(e.target == row) row.sorting_animation_resolver(row)
+    })
+  })
+
+  // Register events on each button.
+  addClickEvent(document.querySelectorAll('.dynamicScaffoldJs-sorter-top'), function(source){
     return document.querySelector('.dynamicScaffoldJs-item-row:first-child')
-  }, function(source, target){
+  }, otherSideAnimationForUp, function(source, target){
     source.parentNode.insertBefore(source, target)
   })
 
-  enableSortToButtons(document.querySelectorAll('.dynamicScaffoldJs-sorter-up'), function(promises, resolvers, source, target){
-    promises.push(new Promise(function(resolve){
-      resolvers.push(resolve)
-      swapAnimation(target, source)
-    }))
-  }, function(source){
+  addClickEvent(document.querySelectorAll('.dynamicScaffoldJs-sorter-up'), function(source){
     return source.previousElementSibling
-  }, function(source, target){
+  }, otherSideAnimationForUp, function(source, target){
     source.parentNode.insertBefore(source, target)
   })
 
-  enableSortToButtons(document.querySelectorAll('.dynamicScaffoldJs-sorter-down'), function(promises, resolvers, source, target){
-    promises.push(new Promise(function(resolve){
-      resolvers.push(resolve)
-      swapAnimation(target, source)
-    }))
-  }, function(source){
+  addClickEvent(document.querySelectorAll('.dynamicScaffoldJs-sorter-down'), function(source){
     return source.nextElementSibling
-  }, function(source, target){
+  }, otherSideAnimationForDown, function(source, target){
     source.parentNode.insertBefore(target, source)
   })
 
-  enableSortToButtons(document.querySelectorAll('.dynamicScaffoldJs-sorter-bottom'), function(promises, resolvers, source, target){
-    do {
-      if(target.previousElementSibling){
-        promises.push(new Promise(function(resolve){
-          resolvers.push(resolve)
-          swapAnimation(target, target.previousElementSibling)
-        }))
-      }
-
-      target = target.previousElementSibling
-      if(target == source) break
-    } while(target.previousElementSibling)
-  }, function(source){
+  addClickEvent(document.querySelectorAll('.dynamicScaffoldJs-sorter-bottom'), function(source){
     return document.querySelector('.dynamicScaffoldJs-item-row:last-child')
-  }, function(source, target){
+  }, otherSideAnimationForDown, function(source, target){
     source.parentNode.insertBefore(source, null)
   })
 })
