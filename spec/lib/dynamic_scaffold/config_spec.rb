e0567d1c47711d660028d153556130dbe898ebc8
Masamoto Miyata <miyata@sincere-co.com>
2018-03-09 11:56:33 +0900

    Fix config spec bugs.

--- spec/lib/dynamic_scaffold/config_spec.rb
+++ spec/lib/dynamic_scaffold/config_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe 'DynamicScaffold::Config' do
     context '#title' do
       it 'should get according to the action.' do
-        config = DynamicScaffold::Config.new Shop
+        config = DynamicScaffold::Config.new(Shop, nil)
         expect(config.title.index(helper)).to eq 'Shop List'
         expect(config.title.edit(helper)).to eq 'Edit Shop'
         expect(config.title.update(helper)).to eq 'Edit Shop'
         expect(config.title.new(helper)).to eq 'Create Shop'
         expect(config.title.create(helper)).to eq 'Create Shop'
 
-        config = DynamicScaffold::Config.new Shop
+        config = DynamicScaffold::Config.new(Shop, nil)
         config.title.name = 'foobar'
         expect(config.title.index(helper)).to eq 'foobar List'
         expect(config.title.edit(helper)).to eq 'Edit foobar'
@@ -21,7 +21,7 @@ RSpec.describe ApplicationHelper, type: :helper do
         expect(config.title.new(helper)).to eq 'Create foobar'
         expect(config.title.create(helper)).to eq 'Create foobar'
 
-        config = DynamicScaffold::Config.new Shop
+        config = DynamicScaffold::Config.new(Shop, nil)
         config.title.name do |_params|
           'Block'
         end
