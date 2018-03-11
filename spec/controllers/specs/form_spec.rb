f6412c742d4b3e7e859a46e0e31ab1ec73b2aeb3
Masamoto Miyata <miyata@sincere-co.com>
2018-03-11 20:13:17 +0900

    Move form block spec.

--- spec/controllers/specs/form_spec.rb
+++ spec/controllers/specs/form_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
-
-RSpec.describe Controls::ShopsBlockController, type: :controller do
-  describe 'block' do
+RSpec.describe SpecsController, type: :controller do
+  delegate :dynamic_scaffold, to: :controller
+  describe 'Form' do
     render_views
     it 'should display additional elements on the page.' do
+      controller.class.send(:dynamic_scaffold, Shop) do |c|
+        c.form.item :block, :block do |form, field|
+          content_tag :div do
+            form.text_field field.name, class: 'form-control'
+          end
+        end
+        c.form.item(:block, :block_with_label).label 'Block with label' do |form, field|
+          form.text_field field.name, class: 'form-control'
+        end
+      end
       get :new, params: { locale: :en }
       body = response.body.delete!("\n").gsub!(/> +</, '><')
       expect(body).to match(/<label>Block<\/label>/)
