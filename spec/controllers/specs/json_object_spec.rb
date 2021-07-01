55f061b6406bd13763144a2f0e3643dd4c2fbdf0
Masamoto Miyata <miyata@sincere-co.com>
2021-07-01 15:41:25 +0900

    Add post json_object spec.

--- spec/controllers/specs/json_object_spec.rb
+++ spec/controllers/specs/json_object_spec.rb
@@ -27,4 +27,35 @@ RSpec.describe SpecsController, type: :controller do
       expect(group.css('input[type="text"][name="shop[data][foobar]"]').length).to eq 1
     end
   end
+  describe 'create' do
+    it 'should register json string at the column' do
+      controller.class.send(:dynamic_scaffold, Shop) do |c|
+        c.form.item(:text_field, :name).label 'Shop Name'
+        c.form.item(:text_field, :category_id)
+        c.form.item(:text_field, :status)
+        c.form.item :json_object, :data do |form|
+          form.item(:text_field, :foobar)
+        end
+        c.form.item :json_object, :data do |form|
+          form.item(:text_field, :quz)
+        end
+      end
+
+      category = FactoryBot.create(:category)
+      post :create, params: { locale: :en, shop: {
+        name: 'foobar',
+        category_id: category.id,
+        status: Shop.statuses.keys.first,
+        data: {
+          'foobar': 'hogehoge',
+          'quz': 'pooooz'
+        }
+      } }
+      shop = Shop.find(assigns(:record).id)
+
+      expect(shop.data).to be_a(ShopData)
+      expect(shop.data.foobar).to eq 'hogehoge'
+      expect(shop.data.quz).to eq 'pooooz'
+    end
+  end
 end
