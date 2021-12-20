567bc0e635509edb6a5182d1ca7ca762c086e1e9
Masamoto Miyata <miyata@sincere-co.com>
2021-12-20 19:13:13 +0900

    Update rails to 6.1.4.4 and make tests pass.

--- lib/dynamic_scaffold/form/item/base.rb
+++ lib/dynamic_scaffold/form/item/base.rb
@@ -173,7 +173,7 @@ module DynamicScaffold
         def errors(form)
           msg = form.object.errors.full_messages_for(proxy_field.name)
           rel = @config.model.reflect_on_all_associations.find {|r| r.foreign_key.to_s == name.to_s }
-          msg.concat(form.object.errors.full_messages_for(rel.name)) if rel.present?
+          msg = msg.dup.concat(form.object.errors.full_messages_for(rel.name)) if rel.present?
           msg
         end
 
